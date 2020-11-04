60fe91c43ce81cfb218ddcb700d4551b573f85d9
= <=>
2020-11-04 15:27:48 +0530

    Solve accessor properties modified test cases

--- js-exercises/accessor-properties/accessorProperties.test.js
+++ js-exercises/accessor-properties/accessorProperties.test.js
@@ -4,8 +4,8 @@ describe('convert to binary', () => {
   test('Should return a binary number', () => {
     const obj = accessorProperties();
     obj.number = 2;
-    expect(obj.binary).toBe('10');
+    expect(obj.binary).toBe(10);
     obj.number = 5;
-    expect(obj.binary).toBe('101');
+    expect(obj.binary).toBe(101);
   });
 });
